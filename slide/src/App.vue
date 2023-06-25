0615cd6c4c84e34c6b0ed5acddfdb6a47973b5b7
tom-larkin <111458319+mr0chill@users.noreply.github.com>
2023-06-25 12:34:31 -0600

    Style updates

--- slide/src/App.vue
+++ slide/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -16,11 +15,8 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
